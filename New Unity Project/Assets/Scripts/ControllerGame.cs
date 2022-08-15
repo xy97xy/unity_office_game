f3604118955274b7a020cb7d6058efd4ab72de7d
Xiaoyu Yan <35710114+xy97xy@users.noreply.github.com>
2022-08-15 00:19:19 -0700

    Iniat

---
+++ New Unity Project/Assets/Scripts/ControllerGame.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerGame : MonoBehaviour
+{
+  public GameObject Player;
+  public GameObject Camera;
+  public GameObject m_userInterface;
+  // Start is called before the first frame update
+  void Start()
+  {
+    Player = GameObject.FindWithTag("Player");
+    Camera = GameObject.FindWithTag("MainCamera");
+    m_userInterface = GameObject.FindWithTag("HUD");
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+      
+  }
+}
